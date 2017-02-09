3dd86536a3d0fe2aed05548daf2716518fafd1e7
nyakerariomokaya <mokayanyakerario@gmail.com>
2017-02-10 01:03:08 +0300

    initial commit

---
+++ http_client.py
@@ -0,0 +1,6 @@
+import requests
+r = requests.get('https://github.com/timeline.json' #makes a get request
+r = requests.post("http://httpbin.org/post") #post request
+r.json()
+[{u'actor_attributes': {u'name': u'Tin..  
+#JSON decoder
